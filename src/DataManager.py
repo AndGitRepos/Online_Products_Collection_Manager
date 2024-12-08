7278213a0e377c087d1733d82e9da21d6b7bc998
AndGitRepos <132986352+AndGitRepos@users.noreply.github.com>
2024-12-08 17:18:47 +0000

    Added structure of DataManager class

--- src/DataManager.py
+++ src/DataManager.py
@@ -1,3 +1,36 @@
+from Collection import Collection
+from typing import List, Dict
+from typing import Any
+
+# Might be best as a static class
+# Each List[Dict[str, Any]] is a collection
+# To store a bunch of collections could use
+# Dict[str, List[Dict[str, Any]]] where first key is name of collection?
+# Have it store data within spreadsheet file where each table is the collections name (key)
 class DataManager:
     def __init__(self):
+        pass
+
+    @staticmethod
+    def loadFromCsv(filename: str) -> List[Collection]:
+        pass
+
+    @staticmethod
+    def saveToCsv(filename: str, collections: List[Collection]):
+        pass
+
+    @staticmethod
+    def loadFromJson(filename: str) -> Collection:
+        pass
+    
+    @staticmethod
+    def saveToJson(filename: str, collection: Collection):
+        pass
+
+    @staticmethod
+    def convertDicitonaryToCollection(dictionary: List[Dict[str, Any]]) -> Collection:
+        pass
+    
+    @staticmethod
+    def convertCollectionToDictionary(collection: Collection) -> List[Dict[str, Any]]:
         pass
