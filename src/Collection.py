00e7f532e9e08698cdd5f1b0026d5e0bf305b8d7
AndGitRepos <132986352+AndGitRepos@users.noreply.github.com>
2024-12-08 16:34:52 +0000

    Added definition of Collection class

--- src/Collection.py
+++ src/Collection.py
@@ -1,3 +1,45 @@
+from Product import Product
+from typing import List
+
 class Collection:
-    def __init__(self):
-        pass
+    def __init__(self, name : str, products : List[Product]) -> None:
+        self._name = name
+        self._products = products
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, name : str) -> None:
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        elif len(name) == 0:
+            raise ValueError("Name cannot be empty")
+        else:
+            self._name = name
+    
+    @property
+    def products(self) -> List[Product]:
+        return self._products
+    
+    @products.setter
+    def products(self, products : List[Product]) -> None:
+        if not isinstance(products, list):
+            raise TypeError("Products must be a list")
+        else:
+            self._products = products
+    
+    def addProduct(self, product : Product) -> None:
+        if not isinstance(product, Product):
+            raise TypeError("Product must be a Product")
+        else:
+            self._products.append(product)
+    
+    def removeProduct(self, product : Product) -> None:
+        if not isinstance(product, Product):
+            raise TypeError("Product must be a Product")
+        elif product not in self._products:
+            raise ValueError("Product not in collection")
+        else:
+            self._products.remove(product)
